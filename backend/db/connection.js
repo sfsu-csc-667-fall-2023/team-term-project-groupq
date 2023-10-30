c157e45963bca714685679fd0f4afdfd5d65b2c3
vickuswan <vuong@mail.sfsu.edu>
2023-10-29 17:01:16 -0700

    database url in connection.js

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,3 +1,3 @@
 const pgp = require("pg-promise")();
-const connection = pgp(process.env.DATABASE_URL);
+const connection = pgp("postgres://victor:9988@localhost:5432/teamq");
 module.exports = connection;
