0ca66428f09e2ca9b43f58504a6c99fdab14f590
vickuswan <vuong@mail.sfsu.edu>
2023-11-20 22:53:32 -0800

    test

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,11 +1,9 @@
 const { isAuthenticated } = require("./is-authenticated");
-const { requestTime } = require("./request-time");
 const { sessionLocals } = require("./session-locals");
 const { viewSessionData } = require("./view-session");
 
 module.exports = {
   isAuthenticated,
-  requestTime,
   sessionLocals,
   viewSessionData,
 };
