7cb161d75559a08d65a574d95e117f28f435c024
ayana <ayana5102001@yahoo.com>
2023-12-09 00:23:38 -0800

    updated game view

--- frontend/games/user_socket.js
+++ frontend/games/user_socket.js
@@ -1,68 +1,93 @@
 import { io } from "socket.io-client";
 import * as GAME_CONSTANTS from "../../constants/games";
-import { userSocketId, cardTemplate, playerOneHandContainer, otherHandContainers } from "./page-data"
-
+import {
+  userSocketId,
+  cardTemplate,
+  playerOneHandContainer,
+  otherHandContainers,
+} from "./page-data";
 
 let socket;
 
 const configure = () => {
   socket = io({ query: { id: userSocketId } });
 
-  socket.on(GAME_CONSTANTS.HAND_UPDATED, ({ user_id, hand, ready_count, simplifiedPlayers }) => {
+  socket.on(
+    GAME_CONSTANTS.HAND_UPDATED,
+    ({ user_id, hand, ready_count, simplifiedPlayers }) => {
+      const filteredPlayers = simplifiedPlayers.filter(
+        (player) => player.user_id >= 0,
+      );
 
-    const filteredPlayers = simplifiedPlayers.filter(player => player.user_id >= 0);
-
-    let num = 0;
-    filteredPlayers.forEach(({ user_id: playerId , chip_count, username }) => {
-      if (playerId === user_id) {
-        updatePlayerHand(hand, chip_count, username);
-      }
-      else {
-        updateHiddenHand(otherHandContainers[num], hand, chip_count, username);
-        num++;
-      }
-    });
-    
-    num = 0;
-    if (ready_count >= 2) {
-      filteredPlayers.forEach(({ user_id: filteredUserId, current_player: filteredCurrentPlayer}, index) => {
-
-        if (filteredUserId == user_id && filteredCurrentPlayer == 0) {
-          const circle1 = document.createElement("div");
-          circle1.classList.add(`dot`);
-          playerOneHandContainer.appendChild(circle1);
+      let num = 0;
+      filteredPlayers.forEach(({ user_id: playerId, chip_count, username }) => {
+        if (playerId === user_id) {
+          updatePlayerHand(hand, chip_count, username);
+        } else {
+          updateHiddenHand(
+            otherHandContainers[num],
+            hand,
+            chip_count,
+            username,
+          );
+          num++;
         }
-        else if (filteredUserId == user_id && filteredCurrentPlayer > 0) {
-          const circle1 = document.createElement("div");
-          circle1.classList.add(`dotHidden`);
-          playerOneHandContainer.appendChild(circle1);
-        }
-        else {
-          if (filteredCurrentPlayer == 0) {
-            const circle2 = document.createElement("div");
-            circle2.classList.add(`dot`);
-            otherHandContainers[num].appendChild(circle2);
-            num++
-          }
-        }      
       });
-    }
-  })
 
-  console.log("This is socketID = ", userSocketId)
+      num = 0;
+      if (ready_count >= 2) {
+        filteredPlayers.forEach(
+          (
+            { user_id: filteredUserId, current_player: filteredCurrentPlayer },
+            index,
+          ) => {
+            if (filteredUserId == user_id && filteredCurrentPlayer == 0) {
+              const circle1 = document.createElement("div");
+              circle1.classList.add(`dot`);
+              playerOneHandContainer.appendChild(circle1);
+            } else if (filteredUserId == user_id && filteredCurrentPlayer > 0) {
+              const circle1 = document.createElement("div");
+              circle1.classList.add(`dotHidden`);
+              playerOneHandContainer.appendChild(circle1);
+            } else {
+              if (filteredCurrentPlayer == 0) {
+                const circle2 = document.createElement("div");
+                circle2.classList.add(`dot`);
+                otherHandContainers[num].appendChild(circle2);
+                num++;
+              }
+            }
+          },
+        );
+      }
+    },
+  );
+
+  console.log("This is socketID = ", userSocketId);
   console.log("User socket configured");
-  return Promise.resolve(socket)
+  return Promise.resolve(socket);
 };
 
 const updatePlayerHand = (cardList, chip_count, user_id) => {
-
   playerOneHandContainer.innerHTML = "";
 
   const seatPosition = String(user_id);
-  const p = document.createElement("p");
-  p.textContent = `Player ${seatPosition}, chip_count = ${chip_count}`;
+  //const p = document.createElement("p");
+  //p.textContent = `Player ${seatPosition}, Total Chip: $${chip_count}`;
 
-  playerOneHandContainer.appendChild(p);
+  const playerInfoSpan = document.createElement("span");
+  playerInfoSpan.textContent = `Player ${seatPosition}`;
+
+  const lineBreak = document.createElement("br");
+
+  const totalChipSpan = document.createElement("span");
+  totalChipSpan.textContent = `Total Chip: $${chip_count}`;
+
+  playerDiv.appendChild(playerInfoSpan);
+  playerDiv.appendChild(lineBreak);
+  playerDiv.appendChild(totalChipSpan);
+
+  //playerOneHandContainer.appendChild(p);
 
   cardList.forEach(({ suit, number }, index) => {
     const container = cardTemplate.content.cloneNode(true);
@@ -76,7 +101,6 @@ const updatePlayerHand = (cardList, chip_count, user_id) => {
 };
 
 const updateHiddenHand = (container, cardList, chip_count, user_id) => {
-
   container.innerHTML = "";
 
   const seatPosition = String(user_id);
